0990474d8b0976818830ae7b3fafda0a9657c5d2
Akinyemi Olumide Emmannuel <akinolu52@gmail.com>
2020-12-25 10:29:25 +0100

    changed to JS

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 const localStorage = {
-    set: function (key: string, value: string) {
+    set: function (key, value) {
         window.localStorage[key] = value;
         return window.localStorage[key];
     },
-    get: function (key: string, defaultValue?: any) {
+    get: function (key, defaultValue= null) {
         return window.localStorage[key] || defaultValue;
     },
-    setObject: function (key: string, value: object) {
+    setObject: function (key, value) {
         window.localStorage[key] = JSON.stringify(value);
         return window.localStorage[key];
     },
-    getObject: function (key: string, defaultValue?: any) {
+    getObject: function (key, defaultValue= null) {
         return JSON.parse(window.localStorage[key] || defaultValue || '{}');
     },
     clear: function () {
         return window.localStorage.clear();
     },
-    remove: function (key: string) {
+    remove: function (key) {
         return window.localStorage.removeItem(key);
     }
 };
 
 const sessionStorage = {
-    set: function (key: string, value: string) {
+    set: function (key, value) {
         window.sessionStorage[key] = value;
         return window.sessionStorage[key];
     },
-    get: function (key: string, defaultValue?: any) {
+    get: function (key, defaultValue= null) {
         return window.sessionStorage[key] || defaultValue;
     },
-    setObject: function (key: string, value: any) {
+    setObject: function (key, value) {
         window.sessionStorage[key] = JSON.stringify(value);
         return window.sessionStorage[key];
     },
-    getObject: function (key: string, defaultValue?: any) {
+    getObject: function (key, defaultValue= null) {
         return JSON.parse(window.sessionStorage[key] || defaultValue || '{}');
     },
     clear: function () {
         return window.sessionStorage.clear();
     },
-    remove: function (key: string) {
+    remove: function (key) {
         return window.sessionStorage.removeItem(key);
     }
 };
